9aa57f978f33366f8556f922896bbab34dbd7b8b
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2019-06-14 09:47:59 +0100

    Increment Version
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- Koha/Plugin/Com/PTFSEurope/CapitaPayments.pm
+++ Koha/Plugin/Com/PTFSEurope/CapitaPayments.pm
@@ -31,7 +31,7 @@ use XML::Compile::SOAP11;
 use XML::Compile::Transport::SOAPHTTP;
 
 ## Here we set our plugin version
-our $VERSION = "00.00.05";
+our $VERSION = "00.00.06";
 our $debug   = 0;
 
 ## Here is our metadata, some keys are required, some are optional
@@ -39,9 +39,9 @@ our $metadata = {
     name            => 'Capita Online Payments Plugin',
     author          => 'Martin Renvoize',
     date_authored   => '2018-06-13',
-    date_updated    => "2018-06-13",
+    date_updated    => "2019-06-09",
     minimum_version => '17.11.00.000',
-    maximum_version => '17.11.18.000',
+    maximum_version => '18.11.06.000',
     version         => $VERSION,
     description     => 'This plugin implements online payments using '
       . 'Capita Educations payments platform.',
